eabf7682f4127b6495745bd126a1ac7314f7f2ba
Trolofoks <trolofoks@gmail.com>
2023-01-26 08:22:38 +0700

    3

--- data/src/main/java/com/example/data/repository/AccountRepositoryImplementation.kt
+++ data/src/main/java/com/example/data/repository/AccountRepositoryImplementation.kt
@@ -1,5 +1,6 @@
 package com.example.data.repository
 
+import android.util.Log
 import com.example.data.accountstorage.model.AccountModel
 import com.example.data.accountstorage.sqlite.AccountDataBase
 import com.example.domain.model.AccountMidModel
@@ -23,8 +24,14 @@ class AccountRepositoryImplementation(private val dataBase: AccountDataBase):Acc
     override fun getAllAccounts(): List<AccountMidModel> {
         val listFlow = dataBase.getDao().getAllAccounts()
         val accountMidModelArrayList = arrayListOf<AccountMidModel>()
+        Log.d("MyLog","start")
+
         listFlow.onEach { list ->
+            Log.d("MyLog","listflow")
+
             list.forEach {
+                Log.d("MyLog","list")
+
                 accountMidModelArrayList.add(
                     AccountMidModel(
                         name = it.name,
@@ -33,8 +40,11 @@ class AccountRepositoryImplementation(private val dataBase: AccountDataBase):Acc
                         password = it.password
                     )
                 )
+                Log.d("MyLog","added")
             }
+            Log.d("MyLog","$accountMidModelArrayList")
         }
+        Log.d("MyLog","$accountMidModelArrayList")
         return accountMidModelArrayList.toList()
 
 //        dataBase.getDao().getAllAccounts().asLiveData().observe(this, Observer { list ->

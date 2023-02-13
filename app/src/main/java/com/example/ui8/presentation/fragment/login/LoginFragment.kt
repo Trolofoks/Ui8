b034a0fa7069d7feb6b6362a9da07a5316a5c7b9
Trolofoks <Trolofoks@gmail.com>
2023-02-13 13:31:03 +0700

    Version 2.1 Koin not work

--- app/src/main/java/com/example/ui8/presentation/fragment/login/LoginFragment.kt
+++ app/src/main/java/com/example/ui8/presentation/fragment/login/LoginFragment.kt
@@ -1,16 +1,16 @@
 package com.example.ui8.presentation.fragment.login
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import com.example.domain.model.NumberAndPassModel
 import com.example.ui8.presentation.BaseFragment
 import com.example.ui8.R
 import com.example.ui8.databinding.FragmentLoginBinding
+import kotlinx.coroutines.flow.collect
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate){
@@ -21,15 +21,22 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::i
         super.onViewCreated(view, savedInstanceState)
         controller = findNavController()
 
-        vm.resultLiveList.observe(viewLifecycleOwner, Observer {
-            binding.apply {
-                if (it){
-                    controller.navigate(R.id.mainPageFragment)
-                } else {
-                    outlinedTextFieldPassword.helperText = getString(R.string.wrong_login_data)
+
+        lifecycleScope.launchWhenCreated {
+            vm.correctFlow.collect() {correct ->
+                if (correct != null){
+                    binding.apply {
+                        if (correct){
+                            controller.navigate(R.id.mainPageFragment)
+                        } else {
+                            outlinedTextFieldPassword.helperText = getString(R.string.wrong_login_data)
+                        }
+                    }
                 }
+
             }
-        })
+        }
+
         binding.buttonLogin.setOnClickListener {
             vm.getAccountByNameAndPass(nameAndPass = NumberAndPassModel(
                 number = binding.textImputEditNumber.text.toString(),
--- app/src/main/java/com/example/ui8/presentation/fragment/login/LoginViewModel.kt
+++ app/src/main/java/com/example/ui8/presentation/fragment/login/LoginViewModel.kt
@@ -5,19 +5,22 @@ import androidx.lifecycle.*
 import com.example.domain.model.IdModel
 import com.example.domain.model.NumberAndPassModel
 import com.example.domain.model.UserSigned
-import com.example.domain.usecase.GetAllAccountsUseCase
 import com.example.domain.usecase.GetIdByNumberAndPasswordUseCase
 import com.example.domain.usecase.SaveSignedUseCase
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+
 
 class LoginViewModel(
     private val getIdByNumberAndPasswordUseCase: GetIdByNumberAndPasswordUseCase,
     private val saveSignedUseCase: SaveSignedUseCase
 ) : ViewModel() {
 
-
-    private val liveDataList = MutableLiveData<Boolean>()
-    val resultLiveList : LiveData<Boolean> = liveDataList
+    private val _correctFlow = MutableStateFlow<Boolean?>()
+    val correctFlow : SharedFlow<Boolean?> = _correctFlow
 
 
     fun getAccountByNameAndPass(nameAndPass: NumberAndPassModel){
@@ -28,11 +31,14 @@ class LoginViewModel(
     }
     private fun getResult(id: IdModel){
         if (id.id.isNotEmpty()){
-            liveDataList.value = true
+            TODO("переписать на shared")
+            _correctFlow.value = true
             saveSignedUseCase.execute(UserSigned(id.id))
             Log.d("MyLogTest","$id")
         } else {
-            liveDataList.value = false
+            viewModelScope.launch {
+                _correctFlow.emit(false)
+            }
         }
     }
 }
